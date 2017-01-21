cce99478ce7f9f51e2183765b9dfd02d16b10304
mwvdev <mwvdev@users.noreply.github.com>
2017-01-21 23:07:50 +0100

    Added skeleton project.

---
+++ src/main/java/mwvdev/BeRightThereApplication.java
@@ -0,0 +1,12 @@
+package mwvdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeRightThereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeRightThereApplication.class, args);
+	}
+}
---
+++ src/main/java/mwvdev/controller/LocationController.java
@@ -0,0 +1,10 @@
+package mwvdev.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LocationController {
+
+    
+
+}
