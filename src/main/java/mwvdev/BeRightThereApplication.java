208e7967e4609b14007f8d3f175a0af8e4c8aff2
mwvdev <mwvdev@users.noreply.github.com>
2018-03-28 23:22:41 +0200

    Upgraded to Spring Boot 2.0.0.

--- src/main/java/mwvdev/BeRightThereApplication.java
+++ src/main/java/mwvdev/BeRightThereApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class BeRightThereApplication {
@@ -16,7 +15,7 @@ public class BeRightThereApplication {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
+        return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**");
