6c879eed43a382794464ed63a59dfa2d78805eef
mwvdev <mwvdev@users.noreply.github.com>
2019-08-31 15:46:50 +0200

    Disabled origin checks on websockets.

--- src/main/java/mwvdev/brt/configuration/WebSocketConfiguration.java
+++ src/main/java/mwvdev/brt/configuration/WebSocketConfiguration.java
@@ -18,7 +18,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/berightthere").withSockJS();
+        registry.addEndpoint("/berightthere").setAllowedOrigins("*").withSockJS();
     }
 
 }
